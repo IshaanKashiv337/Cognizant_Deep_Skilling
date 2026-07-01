2bac34fa16c79e4e0e1020f2850bb0ace5903099
Ishaan Kashiv <ishaankashiv@gmail.com>
2026-07-01 10:16:50 +0530

    Add cursor to update VIP customers based on balance

--- Week-2/Control Structures/Scenario2.sql
+++ Week-2/Control Structures/Scenario2.sql
@@ -1,1 +1,16 @@
 
+DECLARE
+    CURSOR c IS
+        SELECT CustomerID, Balance
+        FROM Customers;
+BEGIN
+    FOR r IN c LOOP
+        IF r.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = TRUE
+            WHERE CustomerID = r.CustomerID;
+        END IF;
+    END LOOP;
+    COMMIT;
+END;
+/
