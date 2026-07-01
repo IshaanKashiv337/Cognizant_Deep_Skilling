4a467a9ef71433f2205f1977005bd897998ca577
Ishaan Kashiv <ishaankashiv@gmail.com>
2026-07-01 10:21:05 +0530

    Update Creating_Tables.sql

--- Week-2/Control Structures/Creating_Tables.sql
+++ Week-2/Control Structures/Creating_Tables.sql
@@ -1,2 +1,17 @@
 
 
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    Age NUMBER,
+    Balance NUMBER,
+    IsVIP VARCHAR2(5)
+);
+
+CREATE TABLE Loans (
+    LoanID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    InterestRate NUMBER,
+    DueDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
