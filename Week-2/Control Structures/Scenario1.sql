e0f3604a706f348d749dd270619124ae87f32712
Ishaan Kashiv <ishaankashiv@gmail.com>
2026-07-01 10:16:32 +0530

    Add cursor to update interest rates for seniors

--- Week-2/Control Structures/Scenario1.sql
+++ Week-2/Control Structures/Scenario1.sql
@@ -1,2 +1,16 @@
-
+DECLARE
+    CURSOR c IS
+        SELECT CustomerID, Age
+        FROM Customers;
+BEGIN
+    FOR r IN c LOOP
+        IF r.Age > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = r.CustomerID;
+        END IF;
+    END LOOP;
+    COMMIT;
+END;
+/
 
