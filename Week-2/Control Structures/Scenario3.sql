93cfb80d5fba07ec7b425929c27a02259b9d81e6
Ishaan Kashiv <ishaankashiv@gmail.com>
2026-07-01 10:20:02 +0530

    Update Scenario3.sql

--- Week-2/Control Structures/Scenario3.sql
+++ Week-2/Control Structures/Scenario3.sql
@@ -1,2 +1,18 @@
 
 
+BEGIN
+    FOR r IN (
+        SELECT c.Name, l.LoanID, l.DueDate
+        FROM Customers c
+        JOIN Loans l
+        ON c.CustomerID = l.CustomerID
+        WHERE l.DueDate BETWEEN SYSDATE AND SYSDATE + 30
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Loan ' || r.LoanID ||
+            ' for ' || r.Name ||
+            ' is due on ' || TO_CHAR(r.DueDate, 'DD-MON-YYYY')
+        );
+    END LOOP;
+END;
+/
